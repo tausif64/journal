473aa8f5e76eb30e2de7b5935674433ae84753ea
tausif64 <developer.642003@gmail.com>
2025-12-06 13:48:14 +0530

    role based auth

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,7 +1,8 @@
 import { createAuthClient } from "better-auth/react";
-import { adminClient } from "better-auth/client/plugins";
+import { inferAdditionalFields } from "better-auth/client/plugins";
+import { auth } from "./auth";
 
 export const authClient = createAuthClient({
   baseURL: "http://localhost:3000",
-  // plugins: [adminClient()],
+  plugins: [inferAdditionalFields<typeof auth>()],
 });
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,6 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-// import { admin } from "better-auth/plugins";
+
 
 import { prisma } from "./prisma";
 
@@ -12,8 +12,11 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
-  // plugins:[
-  //   admin(),
-  // ]
+  additionalFields: {
+    role: {
+      type: "string",
+      input: false
+    }
+  }
 });
 
