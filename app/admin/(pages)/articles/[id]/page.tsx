9c25976e97ccb1f967b4db33dcf1a99a0017903b
tausif64 <developer.642003@gmail.com>
2026-01-10 19:19:38 +0530

    debugging

--- app/admin/(pages)/articles/[id]/page.tsx
+++ app/admin/(pages)/articles/[id]/page.tsx
@@ -2,13 +2,13 @@
 
 import { useParams } from "next/navigation";
 
-export default function Page() {
-  const { id } = useParams<{ id: string }>();
+export default function DebugArticlePage() {
+  const params = useParams();
 
   return (
     <div className="p-6">
-      <h1>Article ID</h1>
-      <pre>{id}</pre>
+      <h1>Article page works</h1>
+      <pre>{JSON.stringify(params, null, 2)}</pre>
     </div>
   );
 }
